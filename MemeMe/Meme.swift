0b177d78296f9d9c7fcda125bfa82e4d0b148b80
Victor Guerra <vguerra@gmail.com>
2015-04-11 12:58:16 +0200

    Adding Screen for viewing detailed meme image.

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,11 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Victor Guerra on 10/04/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+struct Meme {
+    
+}
---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Victor Guerra on 10/04/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController : UIViewController {
+
+
+}

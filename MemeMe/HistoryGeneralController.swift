2a7ff008bfdbc0f0c1bee7a0d2edb861d543e73a
Victor Guerra <vguerra@gmail.com>
2015-04-12 03:08:31 +0200

    Factored out common code between Table View and Collection View Scene. Completing handling of Collection View Controller.

---
+++ MemeMe/HistoryGeneralController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryGeneralController.swift
+//  MemeMe
+//
+//  Created by Victor Guerra on 12/04/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class HistoryGeneralController : UIViewController {
+    var memes : [Meme]!
+
+    func addShowEditorButton() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add,
+            target: self, action: "showMemeEditor")
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    func fetchMemesFromAppDelegate() {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        memes = appDelegate.memes
+    }
+    
+    func showMemeEditor() {
+        let memeEditorController = self.storyboard!.instantiateViewControllerWithIdentifier("MemeEditorViewController") as! MemeEditorViewController
+        self.presentViewController(memeEditorController, animated: true, completion: nil)
+    }
+
+}
--- MemeMe/HistoryTableViewController.swift
+++ MemeMe/HistoryTableViewController.swift
@@ -8,32 +8,36 @@
 
 import UIKit
 
-class HistoryTableViewController : UITableViewController {
-    var memes : [Meme]!
-    
+class HistoryTableViewController : HistoryGeneralController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var sentMemesTableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add,
-            target: self, action: "showMemeEditor")
-        self.navigationItem.rightBarButtonItem = addButton
+        addShowEditorButton()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
+        fetchMemesFromAppDelegate()
+        sentMemesTableView.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let memeCell = tableView.dequeueReusableCellWithIdentifier("memeTableCell") as! MemeTableViewCell
+        let meme = memes![indexPath.row]
+        memeCell.memeLabel.text = meme.topText + meme.bottomText
+        memeCell.memeImage.image = meme.memedImage
+        return memeCell
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let detailViewController = self.storyboard?.instantiateViewControllerWithIdentifier("MemeDetailViewController") as! MemeDetailViewController
+        detailViewController.meme = memes[indexPath.row]
         
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        memes = appDelegate.memes
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let memesCount = memes?.count {
-            return memesCount
-        }
-        return 0
-    }
-    
-    func showMemeEditor() {
-        let memeEditorController = self.storyboard!.instantiateViewControllerWithIdentifier("MemeEditorViewController") as! MemeEditorViewController
-        self.presentViewController(memeEditorController, animated: true, completion: nil)
+        self.navigationController!.pushViewController(detailViewController, animated: true)
     }
 }

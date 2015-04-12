8ea2ba8f5daa7fadf201fa0b7a97970f52b835ec
Victor Guerra <vguerra@gmail.com>
2015-04-12 03:09:58 +0200

    Adding custom Meme View Cell classes for Table View and Collection View.

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Victor Guerra on 12/04/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell : UICollectionViewCell {
+    @IBOutlet weak var memeImage: UIImageView!
+}
---
+++ MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Victor Guerra on 12/04/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell : UITableViewCell {
+    
+    @IBOutlet weak var memeLabel: UILabel!
+    @IBOutlet weak var memeImage: UIImageView!
+}

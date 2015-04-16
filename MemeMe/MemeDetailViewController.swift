1b52982250b0077affd53bb6da67bff2de37c859
Victor Guerra <vguerra@gmail.com>
2015-04-16 22:26:15 +0200

    Details scene knows how to delete now a Meme, asking for your permission of course.

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -10,15 +10,42 @@ import UIKit
 
 class MemeDetailViewController : UIViewController {
   var meme : Meme!
+  var memeIndex : Int!
+  let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+  
   @IBOutlet weak var memeImage: UIImageView!
   
+  // MARK: View life cycle
   override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(true)
+    super.viewWillAppear(false)
     memeImage.image = meme.memedImage
   }
   
+  // MARK: Actions
+  @IBAction func deleteMeme(sender: AnyObject) {
+    weak var wself: MemeDetailViewController? = self
+    
+    let alertController = UIAlertController(title: "Delete this Meme?", message: "Sure?", preferredStyle: .Alert)
+    
+    let cancelAction = UIAlertAction(title: "No", style: .Cancel, handler: nil)
+    alertController.addAction(cancelAction)
+
+    let confirmAction = UIAlertAction(title: "Yes, delete!", style: .Default) { _  in
+      if let sself = wself {
+        sself.appDelegate.memes.removeAtIndex(sself.memeIndex)
+        sself.navigationController?.popViewControllerAnimated(true)
+      }
+    }
+
+    alertController.addAction(confirmAction)
+    self.presentViewController(alertController, animated: true, completion: nil)
+  }
+  
+  // MARK: View configuration
   override func prefersStatusBarHidden() -> Bool {
     return true
   }
+
+  
   
 }
